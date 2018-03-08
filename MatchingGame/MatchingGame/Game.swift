776c981707e8c9d9403b345dd783e10789d33854
Nainggolan <Nainggolan@github.com>
2018-03-08 12:42:01 +0800

    动作关联了第一个按钮

---
+++ MatchingGame/MatchingGame/Game.swift
@@ -0,0 +1,15 @@
+//
+//  Game.swift
+//  MatchingGame
+//
+//  Created by 许玥 on 2018/3/8.
+//  Copyright © 2018年 许玥. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class GameSceneViewController: UIViewController{
+    
+    @IBAction func tileClicked(_ sender: Any) {
+    }
+}
